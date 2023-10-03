8fed36c5fdabf7e7e5fc0bfcb6fc8aa6595478fe
taeyeon2280 <52042235+taeyeon2280@users.noreply.github.com>
2023-10-03 18:55:40 +0900

    Add files via upload
    2,3,4까지 합친 상태입니다!

---
+++ gdscpro2/script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".swiper", {
+    // centerSlide
+    slidesPerView: "1",
+    centeredSlides: true,
+    spaceBetween: 19.67,
+    centeredSlides: true,
+  
+    //loop
+    loop: false,
+  
+    //nextSlide
+    mousewheel: true,
+  
+    //slideSpeed
+    speed: 1000,
+  });
