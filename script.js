c78b786651ec2fb90e9c0687af4a42925b85f441
kaswhy <sxshx818@naver.com>
2024-02-25 21:48:03 +0900

    style: 원페이지 스크롤 3차 수정

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const swiper = new Swiper(".swiper", {
+const swiper_background = new Swiper("#background", {
+  direction: "vertical",
+
+  // centerSlide
+  slidesPerView: 1,
+  centeredSlides: false,
+  spaceBetween: 0,
+
+  //loop
+  loop: false,
+
+  //nextSlide
+  mousewheel: true,
+
+  //slideSpeed
+  speed: 1500,
+});
+
+const swiper = new Swiper("#page2_swiper", {
   // centerSlide
   slidesPerView: "1.1",
   centeredSlides: false,
@@ -28,49 +46,50 @@ window.addEventListener("scroll", function () {
   }
 });
 
-let idx = 0;
-let startY;
-let isScrolling = false;
+// let idx = 0;
+// let startY;
+// let isScrolling = false;
 
-const select = document.querySelector(".background");
-const inner = select.querySelectorAll(".page1, .page2, .page3, .page4");
+// const select = document.querySelector(".background");
+// const inner = select.querySelectorAll(".page1, .page2, .page3, .page4");
 
-$(window).on("touchstart", function (e) {
-  startY = e.originalEvent.touches[0].clientY;
-});
+// $(window).on("touchstart", function (e) {
+//   startY = e.originalEvent.touches[0].clientY;
+// });
 
-$(window).on("touchmove", function (e) {
-  if (isScrolling) return; // 이미 스크롤 중이면 무시
-  isScrolling = true;
+// $(window).on("touchmove", function (e) {
+//   if (isScrolling) return; // 이미 스크롤 중이면 무시
+//   isScrolling = true;
 
-  const scrollDistance = e.originalEvent.changedTouches[0].clientY - startY;
+//   const scrollDistance = e.originalEvent.changedTouches[0].clientY - startY;
 
-  const htmlFontSize = parseFloat($("html").css("font-size"));
-  const remScrollDistance = scrollDistance / htmlFontSize;
-  const remInnerHeight = inner[idx].clientHeight / htmlFontSize;
+//   const htmlFontSize = parseFloat($("html").css("font-size"));
+//   const remScrollDistance = scrollDistance / htmlFontSize;
+//   const remInnerHeight = inner[idx].clientHeight / htmlFontSize;
 
-  if (remScrollDistance > remInnerHeight / 60) {
-    idx = Math.max(idx - 1, 0);
-  } else if (-remScrollDistance > remInnerHeight / 60) {
-    idx = Math.min(idx + 1, inner.length - 1);
-  } else {
-    idx = idx;
-  }
+//   if (remScrollDistance > remInnerHeight / 60) {
+//     idx = Math.max(idx - 1, 0);
+//   } else if (-remScrollDistance > remInnerHeight / 60) {
+//     idx = Math.min(idx + 1, inner.length - 1);
+//   } else {
+//     idx = idx;
+//   }
 
-  $("html,body")
-    .stop()
-    .animate(
-      {
-        scrollTop: $(inner[idx]).offset().top, // 다음 페이지의 시작 위치로 스크롤
-      },
-      600,
-      function () {
-        isScrolling = false;
-      }
-    );
+//   $("html,body")
+//     .stop()
+//     .animate(
+//       {
+//         scrollTop: $(inner[idx]).offset().top, // 다음 페이지의 시작 위치로 스크롤
+//       },
+//       600,
+//       function () {
+//         isScrolling = false;
+//       }
+//     );
 
-  // 스크롤 감쇠
-  setTimeout(function () {
-    isScrolling = false;
-  }, 100);
-});
+//   // 스크롤 감쇠
+//   setTimeout(function () {
+//     isScrolling = false;
+//   }, 100);
+// });
+
